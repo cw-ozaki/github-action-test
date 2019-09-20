fdef43668297dc8068209e544c47383846bf2bbb
cw-ozaki <log@logn.in>
2019-09-20 15:04:40 +0900

    ..............

--- up/entrypoint.sh
+++ up/entrypoint.sh
@@ -2,11 +2,18 @@
 
 cd "${MOD_ACTION_WORKING_DIR:-.}"
 
-echo "## user: ${GITHUB_ACTOR}@users.noreply.github.com"
-
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
 
+echo "#########################################"
+
 git config --get-regexp '.*'
+
+echo "#########################################"
+
+git config --get-all http.https://github.com/cw-ozaki/github-action-test.extraheader
+git config --get-all http.proxy
+
+echo "#########################################"
 
 exec mod up "$@"
