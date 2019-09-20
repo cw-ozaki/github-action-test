eb4059e6f0c649cb5d2006f93aa3769971e9c826
cw-ozaki <log@logn.in>
2019-09-20 12:51:13 +0900

    ...........

--- up/entrypoint.sh
+++ up/entrypoint.sh
@@ -2,6 +2,8 @@
 
 cd "${MOD_ACTION_WORKING_DIR:-.}"
 
+echo "## user: ${GITHUB_ACTOR}@users.noreply.github.com"
+
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config --global user.name "${GITHUB_ACTOR}"
 env
