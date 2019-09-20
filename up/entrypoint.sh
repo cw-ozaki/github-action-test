8c52767e8198f77c6616bd8d6aa462ad38c7c8a3
cw-ozaki <log@logn.in>
2019-09-20 14:43:14 +0900

    ............

--- up/entrypoint.sh
+++ up/entrypoint.sh
@@ -4,8 +4,7 @@ cd "${MOD_ACTION_WORKING_DIR:-.}"
 
 echo "## user: ${GITHUB_ACTOR}@users.noreply.github.com"
 
-git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-git config --global user.name "${GITHUB_ACTOR}"
-env
+git config --global user.email "ozaki@@chatwork.com"
+git config --global user.name "cw-ozaki"
 
 exec mod up "$@"
